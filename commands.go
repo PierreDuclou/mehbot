64d67fa576edad322fe548af7138c8d6a89af3eb
Pierre Duclou <pierre.duclou-guerinot@protonmail.com>
2019-07-29 17:33:01 +0200

    add: help command

--- commands.go
+++ commands.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/bwmarrin/discordgo"
 )
 
+var usage = &discordgo.MessageEmbed{
+	Title: "Liste des commandes disponibles",
+	Color: 0x4bfcc4,
+}
+
+func init() {
+	cmdnames := discordgo.MessageEmbedField{Name: "Commande", Inline: true}
+	cmddesc := discordgo.MessageEmbedField{Name: "Description", Inline: true}
+
+	for _, command := range commands {
+		cmdnames.Value += fmt.Sprintf("%s%s (%s%s)\n", config.prefix, command.Name, config.prefix, command.Alias)
+		cmddesc.Value += command.Description + "\n"
+	}
+
+	usage.Fields = []*discordgo.MessageEmbedField{&cmdnames, &cmddesc}
+}
+
 // Command represents a basic command provided to users
 type Command struct {
 	Name        string
 	Alias       string
+	Description string
 	Authroles   []string
 	Session     *discordgo.Session
 	MessageData *discordgo.MessageCreate
@@ -41,10 +60,10 @@ func (c Command) Authorized(s *discordgo.Session, m *discordgo.MessageCreate) bo
 	return false
 }
 
-// GetCommand returns the corresponding Command depending on the given name
+// GetCommand returns a Command instance matching the given name
 func GetCommand(name string, s *discordgo.Session, m *discordgo.MessageCreate) *Command {
 	for _, command := range commands {
-		if name == command.Name || command.Alias == name {
+		if name == command.Name || name == command.Alias {
 			command.Session = s
 			command.MessageData = m
 			return &command
@@ -53,4 +72,36 @@ func GetCommand(name string, s *discordgo.Session, m *discordgo.MessageCreate) *
 	return nil
 }
 
-var commands = []Command{}
+var commands = []Command{
+	Command{
+		Name:        "help",
+		Alias:       "h",
+		Description: "Affiche ce message d'aide",
+		Authroles:   config.baseroles,
+		Run: func(c Command, args []string) {
+			_, err := c.Session.ChannelMessageSendEmbed(c.MessageData.ChannelID, usage)
+
+			if err != nil {
+				log.Println("error sending message embed:", err)
+			}
+		},
+	},
+	Command{
+		Name:        "worms",
+		Alias:       "w",
+		Description: "Prochainement...",
+		Authroles:   config.baseroles,
+		Run: func(c Command, args []string) {
+
+		},
+	},
+	Command{
+		Name:        "youtube",
+		Alias:       "yt",
+		Description: "Prochainement...",
+		Authroles:   config.baseroles,
+		Run: func(c Command, args []string) {
+
+		},
+	},
+}
